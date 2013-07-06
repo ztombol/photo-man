a491125da114d999c58e45c3d9694cb201460ee1
Zoltan Vass <zoltan.tombol@gmail.com>
2013-07-06 12:35:20 +0200

    added dry-run, improved output

--- photo-man.pl
+++ photo-man.pl
@@ -21,7 +21,6 @@ use Pod::Usage;             # Printing usage information
 use File::Glob ':bsd_glob'; # Producing file names
 use File::stat;             # Accessing filesystem metadata
 use File::Copy;
-use File::Basename;
 use File::Path 'make_path';
 use DateTime;
 use DateTime::Format::Strptime;
@@ -42,15 +41,24 @@ my $opt_touch = 0;
 my $arg_touch_tz = '';
 
 # Rename the input file(s) based on the template given as argument. The
-# template can contain 
+# template can contain TODO.
 my $opt_rename = 0;
 my $arg_rename_template = '';
 
+# Sort the input file(s) based on the template given as argument.
+# TODO: template
+my $opt_sort = 0;
+my $arg_sort_template = '';
+
+# If not set, do not change/move anything only display the actions that would
+# be taken if we were to do it for real.
+my $opt_commit = 0;
+
 # Print help information.
-my $opt_help = 0;
+my $opt_help = 'info';
 
 # Set verbosity level.
-my $opt_log_level = 0;
+my $opt_verbose = 0;
 
 
 
@@ -60,24 +68,24 @@ my $opt_log_level = 0;
 
 # Parse options.
 my $parser = Getopt::Long::Parser->new;
-$parser->getoptions('touch=s'  => \&handler_touch,
-                    'rename:s' => \&handler_rename,
-                    'verbose+' => \$opt_log_level,
-                    'help'     => \$opt_help,) or pod2usage(2);
+$parser->getoptions('touch=s' => \&handler_touch,
+                    'sort:s'  => \&handler_sort,
+                    'commit'  => \$opt_commit,
+                    'verbose' => \$opt_verbose,
+                    'help'    => \$opt_help,) or pod2usage(2);
 
-# OPTION: rename
-sub handler_rename {
-    $opt_rename = 1;
-    ( undef, $arg_rename_template ) = @_;
+# Argument handlers.
+sub handler_sort {
+    $opt_sort = 1;
+    ( undef, $arg_sort_template ) = @_;
 }
 
-# OPTION: touch
 sub handler_touch {
     $opt_touch = 1;
     ( undef, $arg_touch_tz ) = @_;
 }
 
-# Check arguments.
+# Check imput files.
 if (@ARGV == 0) {
     # No input files specified.
     print "Error: no input file specified!\n";
@@ -89,23 +97,57 @@ if (@ARGV == 0) {
 # Processing images
 ###############################################################################
 
-# Print details of processing.
-#if (1) {
-#print <<DET;
-#:: Parameters
-#  template = $arg_rename_template
-# time zone = $arg_touch_tz
-#DET
-#}
+# Initialise variables shared among all actions.
+my $exif_tool = new Image::ExifTool;
+my $local_tz = DateTime::TimeZone->new(name => 'local');
+my $img_mtime_parser = DateTime::Format::Strptime->new(
+    pattern   => '%Y:%m:%d %H:%M:%S',
+    time_zone => $arg_touch_tz);
+
+# Print parameters.
+if ($opt_verbose) {
+    my $name_w = 10;
+    print ":: Parameters\n";
+    printf("%*s = %s\n", $name_w, 'mode', $opt_commit ? 'commit changes' : 'dry-run');
+    if ($opt_touch) {
+        printf("%*s = %s\n", $name_w, 'time zone', $arg_touch_tz);
+    }
+    if ($opt_sort) {
+        printf("%*s = %s\n", $name_w, 'template', $arg_sort_template);
+    }
+}
 
 # Processing each file
 print ":: Processing images\n";
-my $exif_tool = new Image::ExifTool;
+
+# Formatting variables.
+# TODO: should be encapsulated in a formatting module.
+my $result_w = 6;
+my $filename_w = 40;
+my $touch_w = 4;
+my $sort_w = 4;
+
+# Print column header.
+if (not $opt_verbose) {
+    # result [filename]
+    printf("%*s", $result_w, '');
+    printf(" [%-*s]", $filename_w, 'filename');
+    printf(" [%*s]", $touch_w, 'time') if $opt_touch;
+    printf(" [%*s]", $sort_w, 'move') if $opt_sort;
+    print "\n";
+}
 
 my $pattern = '{' . join(',', @ARGV) . '}';
 while (my $file = glob("$pattern")) {
-    print "[    ] $file ";
-#    print "\n";
+
+    # Print file name.
+    if ($opt_verbose) {
+        print "-- $file\n";
+    }
+    else {
+        printf("%*s", $result_w, '[    ]');
+        printf(" %-*s", $filename_w+2, $file);
+    }
 
     # Extract metadata from image.
     my $img_meta = $exif_tool->ImageInfo("$file");
@@ -115,72 +157,103 @@ while (my $file = glob("$pattern")) {
         # File system timestamp.
         my $fs_meta = stat($file);
         my $fs_mtime = DateTime->from_epoch(epoch     => $fs_meta->mtime,
-                                            time_zone => 'local');
-
-        # Print timestamp.
-        for ($fs_mtime->time_zone, 'UTC') {
-            $fs_mtime->set_time_zone($_);
-#            print "fs  = [ ", $fs_mtime->time_zone_short_name, " ] ", $fs_mtime->strftime("%F %T %z"), "\n";
-        }
+                                            time_zone => $local_tz);
 
         # EXIF creation timestamp.
-        my $parser = DateTime::Format::Strptime->new(
-            pattern => '%Y:%m:%d %H:%M:%S');
-        my $img_mtime = $parser->parse_datetime( $$img_meta{'CreateDate'} );
-        $img_mtime->set_time_zone($arg_touch_tz);
+        my $img_mtime = $img_mtime_parser->parse_datetime($$img_meta{'CreateDate'});
+        $img_mtime->set_formatter($img_mtime_parser);
 
-        # Print timestamp.
-        for ($img_mtime->time_zone, 'UTC') {
-            $img_mtime->set_time_zone($_);
-#            print "img = [ ", $img_mtime->time_zone_short_name, " ] ", $img_mtime->strftime("%F %T %z"), "\n";
-        }
-
-#        print "-----\n";
-
-        # Compare UTC timestamps.
+        # Set new timestamp if needed.
         if ($img_mtime->truncate(to=>'second') ==
             $fs_mtime->truncate(to=>'second')) {
-#            print "date OK; nothing to do\n";
+
+            if ($opt_verbose) {
+                print "[ -- ] timestamp unchanged\n";
+            }
+            else {
+                printf("  %*s ", $touch_w, '-- ');
+            }
         }
         else {
+            # Convert timestamp to local time zone. So the correct date and
+            # time is stringified when we set the file system timestamp.
+            $img_mtime->set_time_zone($local_tz);
+
             # Print timestamp.
-            for ('local', $arg_touch_tz, 'UTC') {
-                $img_mtime->set_time_zone($_);
-#                print "new = [ ", $img_mtime->time_zone_short_name, " ] ", $img_mtime->strftime("%F %T %z"), "\n";
+            if ($opt_verbose) {
+                print "[    ] timestamp = ", $img_mtime->strftime("%F %T %z"),
+                      " (", $img_mtime->time_zone_short_name, ")";
             }
-            $img_mtime->set_time_zone('local');
+            else {
+                printf("  %*s ", $touch_w, 'DONE');
+            }
 
-            # Set new timestamp.
-            $exif_tool->SetNewValue(FileModifyDate => $img_mtime->date(':') . ' ' . $img_mtime->hms,
-                                    Protected      => 1);
+            # Write timestamp to file if we are not making a dry-run (default).
+            if ($opt_commit) {
+                $exif_tool->SetNewValue(FileModifyDate => $img_mtime,
+                                        Protected      => 1);
 
-            # Write timestamp to file.
-            my $success = $exif_tool->SetFileModifyDate($file);
-#            print "FIXING date; success = $success\n";
+                if (not $exif_tool->SetFileModifyDate($file)) {
+                    print "\r[FAIL]\n";
+                    die "ERROR: failed setting file system timestamp!\n";
+                }
+            }
+
+            # Print result.
+            if ($opt_verbose) {
+                print "\r[DONE]\n";
+            }
         }
 
     }
 
-    # Rename files.
-    if ($opt_rename) {
-
-        # Retrieve EXIF creation timestamp.
-        my $parser = DateTime::Format::Strptime->new(
-            pattern => '%Y:%m:%d %H:%M:%S');
-        my $img_mtime = $parser->parse_datetime($$img_meta{'CreateDate'});
-
-        # Retrieve file extension.
+    # Sort files.
+    if ($opt_sort) {
+        # Construct the new filename by applying the template and appending the
+        # original file extension to the result.
+        my $img_mtime = $img_mtime_parser->parse_datetime($$img_meta{'CreateDate'});
         my ($ext) = $file =~ /\.([^.]+)$/;
+        my $new_file = $img_mtime->strftime($arg_sort_template) . '.' . $ext;
 
-        # Constructing new name.
-        my $new_file = $img_mtime->strftime($arg_rename_template) . '.' . $ext;
+        # Move file if current and new location are not the same.
+        if ($file ne $new_file) {
 
-        # Create location if it doesn't exist yet.
-        my ($nvol, $ndirs, $nfile) = File::Spec->splitpath($new_file);
-        make_path $ndirs;
+            # Print new file name.
+            if ($opt_verbose) {
+                print "[    ] new path = $new_file";
+            }
+            else {
+                printf("  %*s ", $sort_w, 'DONE');
+            }
 
-        # Finally, move the file.
-        move($file, $new_file);
+            # Move/Copy the file if we are not making a dry-run (default).
+            if ($opt_commit) {
+                # Create location if it doesn't exist yet.
+                my ($nvol, $ndirs, $nfile) = File::Spec->splitpath($new_file);
+                make_path $ndirs;
+
+                # Finally, move the file.
+                if (not move($file, $new_file)) {
+                    print "\r[FAIL]\n";
+                    die "ERROR: moving file!\n$!\n"
+                }
+            }
+
+            # Print result.
+            if ($opt_verbose) {
+                print "\r[DONE]\n";
+            }
+        }
+        else {
+            # Target and source path+file are the same.
+            if ($opt_verbose) {
+                print "[ -- ] file not moved";
+            }
+            else {
+                printf("  %*s ", $sort_w, ' -- ');
+            }
+        }
+
     }
 
 #    {
@@ -189,7 +262,12 @@ while (my $file = glob("$pattern")) {
 #        }
 #    }
 
-    print "\r[DONE]\n";
+    if ($opt_verbose) {
+        print "\n";
+    }
+    else {
+        print "\r[DONE]\n";
+    }
 }
 
 
@@ -243,7 +321,8 @@ which the photo was taken has to be specified by TIME_ZONE.
 
 --verbose
 
-produce detailed output
+Produce verbose output.
+
 
 --help
 
