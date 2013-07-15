7eca265e0d25fbb8f73ec0a3b8591196f2146134
Zoltan Vass <zoltan.tombol@gmail.com>
2013-07-15 17:32:26 +0200

    added --force, restructured sort and touch

--- photo-man.pl
+++ photo-man.pl
@@ -52,6 +52,9 @@ my $opt_sort = 0;
 my $arg_sort_op = 'move';
 my $arg_sort_template = '';
 
+# Force destructive operations, e.g. move/copy when target exists.
+my $opt_force = 0;
+
 # If not set, do not change/move anything only display the actions that would
 # be taken if we were to do it for real.
 my $opt_commit = 0;
@@ -72,6 +75,7 @@ my $opt_verbose = 0;
 my $parser = Getopt::Long::Parser->new;
 $parser->getoptions('touch=s' => \&handler_touch,
                     'sort:s'  => \&handler_sort,
+                    'force'   => \$opt_force,
                     'commit'  => \$opt_commit,
                     'verbose' => \$opt_verbose,
                     'help'    => \$opt_help,) or pod2usage(2);
@@ -138,47 +142,36 @@ my $pattern = '{' . join(',', @ARGV) . '}';
 while (my $file = glob("$pattern")) {
 
     # Print file name.
-    if ($opt_verbose) {
-        $header->next("$file");
-    }
-    else {
-        # Leave result empty. We will fill it in later.
-        $record->next('');
-        $record->next($file);
-    }
+    if ($opt_verbose) { $header->next("$file"); }
+    else { $record->next($file); }
 
     # Extract metadata from image.
     my $img_meta = $exif_tool->ImageInfo("$file");
 
     # Set modification timestamp to EXIF creation.
-    if ($opt_touch) {
-        touch({
-            file => $file,
-            img_meta => $img_meta,
-        });
-    }
+    touch({file => $file,
+           img_meta => $img_meta,
+    }) if $opt_touch;
 
     # Sort files.
-    if ($opt_sort) {
-        sort_files({
-            file      => $file,
-            img_meta  => $img_meta,
-            operation => $arg_sort_op,
-        });
-    }
+    sort_file({file      => $file,
+               img_meta  => $img_meta,
+               operation => $arg_sort_op,
+               forced    => $opt_force,
+    }) if $opt_sort; 
 
     # To display all EXIF metadata, uncomment the following block.
     #foreach (keys %$img_meta) {
     #    print "$_ => $$img_meta{$_}\n";
     #}
 
-    # Print after processing file.
-    if ($opt_verbose) {
-        # Nothing to do for verbose mode.
+    # Print overall result of all operations.
+    if ( $opt_verbose) {
+        # Nothing to do for verbose mode. Result of each operation is printed
+        # at the end of the corresponding functions.
     }
     else {
-        # Print overall result.
-        $record->next('DONE');
+        $record->next('done');
     }
 }
 
@@ -195,13 +188,18 @@ while (my $file = glob("$pattern")) {
 sub print_args {
     # Output format.
     my $param = Output::Columnar->new({
-            format => "@>>>>>>>>> = @<\n",
-            groups => "          ^      ",
-        });
+        format => "@>>>>>>>>> = @<\n",
+        groups => "          ^      ",
+    });
 
     # Mode: make changes or just show what would be done.
     $param->next('mode');
     $param->next( $opt_commit ? 'commit changes' : 'dry-run' );
+
+    # Forced: perform destructive and irreversible operations, e.g. overwriting
+    # files while sorting.
+    $param->next('forced');
+    $param->next( $opt_force ? 'yes' : 'no' );
 
     # Timestamp fix.
     if ($opt_touch) {
@@ -218,7 +216,9 @@ sub print_args {
 
 
 # Initialise output by building output formatting strings and instantiating
-# output handling objects.
+# output handling objects. The first value returned is used for printing column
+# headers in normal mode and filenames in verbose mode. The second value
+# returned is for printing details of processing a single file (records).
 #
 # NOTE: This functions accesses `options and arguments' in the global scope!
 #
@@ -230,39 +230,39 @@ sub init_output {
 
     if ($opt_verbose) {
         # Verbose output (vertical). Print operations/messages on their own line.
-        $header_fmt = ":: @<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n";
-        $header_grp = "                                           ";
+        $header_fmt = ":: @<\n";
+        $header_grp = "       ";
         
-        $record_fmt = "[@|||||] @>>>>>>>>>>>> = @<<<<<<<\r[@|||||]\n";
-        $record_grp = "        ^             ^          ^           ";
+        $record_fmt = "[      ] @>>>>>>>>>>>> = @<<<<<<<\r[@|||||]\n";
+        $record_grp = "                      ^          ^           ";
     }
     else {
         # Normal output (horizontal). Print operations/messages on adjecent cells
         # on the same line.
 
         # Base: `[result] [filename]'
-        $header_fmt = "[@|||||] [@<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<]";
-        $header_grp = "        ^                                        ";
+        $header_fmt = "[@|||||] [@<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<]";
+        $header_grp = "        ^                                              ";
      
-        $record_fmt = "[@|||||] @<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<";
-        $record_grp = "        ^                                        ";
+        $record_fmt = "[      ] @<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<";
+        $record_grp = "                                                       ";
         
         # Add timestamp fix: ` [result]'
         if ( $opt_touch ) {
-            $header_fmt .= " [@|||||]";
-            $header_grp .= "^        ";
+            $header_fmt .= " [@|||||||]";
+            $header_grp .= "^          ";
 
-            $record_fmt .= " @|||||||";
-            $record_grp .= "^        ";
+            $record_fmt .= " @|||||||||";
+            $record_grp .= "^          ";
         }
 
         # Add sort fix: ` [result]'
         if ( $opt_sort ) {
-            $header_fmt .= " [@|||||]";
-            $header_grp .= "^        ";
+            $header_fmt .= " [@|||||||]";
+            $header_grp .= "^          ";
 
-            $record_fmt .= " @|||||||";
-            $record_grp .= "^        ";
+            $record_fmt .= " @|||||||||";
+            $record_grp .= "^          ";
         }
 
         # End of record. Add line feed or overall result: `\r[result]\n'.
@@ -294,34 +294,31 @@ sub init_output {
 # @param [in] file      name of the file
 # @param [in] img_meta  EXIF metadata (hash ref)
 sub touch {
-    my ($args) = @_;
+    my ( $args ) = @_;
 
     # Arguments and default values.
-    my $file = $args->{file} || die "[TOUCH] ERROR: no file specified\n";
+    my $file = $args->{file} || die "[touch]: ERROR: no file specified\n";
     my $img_meta = $args->{img_meta};
 
     # File system timestamp.
-    my $fs_meta = stat($file);
-    my $fs_mtime = DateTime->from_epoch(epoch     => $fs_meta->mtime,
-                                        time_zone => $local_tz);
+    my $fs_meta = stat( $file );
+    my $fs_mtime = DateTime->from_epoch(
+        epoch     => $fs_meta->mtime,
+        time_zone => $local_tz,
+    );
 
     # EXIF creation timestamp.
-    my $img_mtime = $img_mtime_parser->parse_datetime($img_meta->{'CreateDate'});
+    my $img_mtime = $img_mtime_parser->parse_datetime(
+        $img_meta->{'CreateDate'}
+    );
     $img_mtime->set_formatter($img_mtime_parser);
 
-    if ( $opt_verbose ) {
-        # Leave result empty. We will fill it in later.
-        $record->next('');
-    }
-    else {
-    }
-
     # Set new timestamp if needed.
-    if ($img_mtime->truncate(to=>'second') ==
-        $fs_mtime->truncate(to=>'second')) {
-        # Timestamp is correct.
+    if ( $img_mtime->truncate( to => 'second' ) ==
+        $fs_mtime->truncate( to => 'second' ) ) {
 
-        if ($opt_verbose) {
+        # Timestamp is correct.
+        if ( $opt_verbose ) {
             $record->next('timestamp unchanged');
             $record->skip_group();
             $record->next('--');
@@ -347,10 +344,12 @@ sub touch {
 
         # Write timestamp to file if we are not making a dry-run (default).
         if ($opt_commit) {
-            $exif_tool->SetNewValue(FileModifyDate => $img_mtime,
-                                    Protected      => 1);
+            $exif_tool->SetNewValue( FileModifyDate => $img_mtime,
+                                     Protected      => 1,
+            );
 
-            if (not $exif_tool->SetFileModifyDate($file)) {
+            if ( not $exif_tool->SetFileModifyDate($file) ) {
+                # Something went wrong.
                 print "\r[FAIL]\n";
                 die "ERROR: failed setting file system timestamp!\n";
             }
@@ -358,69 +357,43 @@ sub touch {
 
         # Print result.
         if ($opt_verbose) {
-            $record->next('DONE');
+            $record->next('done');
         }
     }
 }
 
-sub sort_files {
-    my ($args) = @_;
+# Sort a photo. In this context, sorting means the sorting of a photo album by
+# moving or copying each photo to a specified location potentially based on
+# when the photos were taken. This subroutine performs interpolation on the
+# sorting template and makes sure the file is moved/copied only if the target
+# does not exist, is not the same as the source or overwriting the target is
+# authorised.
+#
+# @param [in] op        operation to perform, `copy' or `move'
+# @param [in] file      path of file to sort
+# @param [in] img_meta  hash ref containing EXIF metadata
+# @param [in] forced    force overwriting target (optional)
+sub sort_file {
+    my ( $args ) = @_;
 
     # Arguments and default values.
-    my $file = $args->{file} || die "[TOUCH] ERROR: no file specified\n";
-    my $img_meta = $args->{img_meta};
     my $op = $args->{operation};
-
+    my $file = $args->{file} || die "[touch]: ERROR: no file specified\n";
+    my $img_meta = $args->{img_meta};
+    my $forced = $args->{forced} || 0;
+    
     # Construct the new filename by applying the template and appending the
     # original file extension to the result.
     my $img_mtime = $img_mtime_parser->parse_datetime($img_meta->{'CreateDate'});
-    my ($ext) = $file =~ /\.([^.]+)$/;
+    my ( $ext ) = $file =~ /\.([^.]+)$/;
     my $new_file = $img_mtime->strftime($arg_sort_template) . '.' . $ext;
     
-    # Print
-    if ( $opt_verbose ) {
-        # Leave result empty. We will fill it in later.
-        $record->next('1');
-    }
-    else {
-    }
 
     # Move file if current and new location are not the same.
-    my $force = 0;
-    if ((not -e $new_file) or $force) {
-
-        # Print new path.
-        if ($opt_verbose) {
-            $record->next('new path');
-            $record->next($new_file);
-        }
-
-        # Move/Copy the file if we are not making a dry-run (default).
-        if ($opt_commit) {
-            # Create location if it doesn't exist yet.
-            my ($nvol, $ndirs, $nfile) = File::Spec->splitpath($new_file);
-            make_path $ndirs;
-
-            # Finally, move the file.
-            # TODO: handle $op
-            if (not move($file, $new_file)) {
-                print "\r[FAIL]\n";
-                die "ERROR: moving file!\n$!\n"
-            }
-        }
-
-        # Print result.
-        if ($opt_verbose) {
-            $record->next('move');
-        }
-        else {
-            $record->next('move');
-        }
-    }
-    elsif ($file eq $new_file) {
-        # Target and source path+file are the same.
-        if ($opt_verbose) {
-            $record->next('file not moved');
+    if ( $file eq $new_file ) {
+        # File is already at the right place (target == source).
+        if ( $opt_verbose ) {
+            $record->next('already sorted');
             $record->skip_group();
             $record->next('--');
         }
@@ -428,6 +401,87 @@ sub sort_files {
             $record->next('--');
         }
     }
+    elsif ( -e $new_file and not $forced ) {
+        # Target file already exists.
+        if ( $opt_verbose ) {
+            $record->next('already exists');
+            $record->next( $new_file );
+            $record->next('--');
+        }
+        else {
+            $record->next('exists');
+        }
+    }
+    else {
+        # We get here if the target file either does not exist or we are
+        # running in forced mode and therefore ready to overwrite target, i.e.
+        # `-e _ or $forced'.
+
+        # Perform operation if we are not doing a dry-run.
+        if ($opt_commit) {
+            if ( _do_sort($op, $file, $new_file) ) {
+                die "[sort] ERROR: performing operation: `$op' forced: `$forced'";
+            }
+        }
+
+        # Print result
+        if ( not $forced ) {
+            # Simplest case: target did not exist.
+            if ( $opt_verbose ) {
+                $record->next( $op );
+                $record->next( $new_file );
+                $record->next('done');
+            }
+            else {
+                $record->next( $op );
+            }
+        }
+        else {
+            # Target was overwritten.
+            if ( $opt_verbose ) {
+                $record->next('overwriting');
+                $record->next( $new_file );
+                $record->next('done');
+            }
+            else {
+                $record->next('overwrite');
+            }
+        }
+    }
+}
+
+
+# Perform the specified `sorting' operation with the given source and target.
+# For more information on what `sorting' means in this context see the
+# `sort_file' subroutine.
+#
+# @param [in] $1  operation to perform ( move | copy )
+# @param [in] $2  file to sort
+# @param [in] $3  target to move/copy to
+# @returns        0 on success, 1 otherwise
+sub _do_sort {
+    my ( $op, $file, $new_file ) = @_;
+
+    # Make sure the directory tree leading up to the target location exists.
+    my ( $nvol, $ndirs, $nfile ) = File::Spec->splitpath( $new_file );
+    make_path $ndirs;
+
+    # Perform action.
+    if ($op eq 'move') {
+        if ( not move($file, $new_file) ) {
+            return 1;
+        }
+    }
+    elsif ($op eq 'copy') {
+        die "TODO: implement copy";
+    }
+    else {
+        # Unrecognised operation.
+        die "[sort]: ERROR: unrecognised operation `$op'";
+    }
+
+    # Operation sucessfully executed.
+    return 0;
 }
 
 
